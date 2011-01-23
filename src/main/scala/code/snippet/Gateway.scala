b8f27f2bfbd67d81682426a064dbd0fc3d0a4d86
Scott Abernethy <scott.abernethy@aviatnet.com>
2011-01-23 15:04:28 +1300

    Cultist can add ro gateway.

--- src/main/scala/code/snippet/Gateway.scala
+++ src/main/scala/code/snippet/Gateway.scala
@@ -15,12 +15,14 @@ class Gateway {
   object share extends RequestVar("")
   object path extends RequestVar("")
   object password extends RequestVar("")
+  object readOnly extends RequestVar(false)
   def add = {
     ClearClearable &
     ".add:host" #> JsCmds.FocusOnLoad(SHtml.text(host.is, t => host(t)) % ("style" -> "width: 250px")) &
     ".add:share" #> (SHtml.text(share.is, t => share(t)) % ("style" -> "width: 250px")) &
     ".add:path" #> (SHtml.text(path.is, t => path(t)) % ("style" -> "width: 250px")) &
     ".add:password" #> (SHtml.password(password.is, t => password(t)) % ("style" -> "width: 250px")) &
+    ".add:mode" #> (SHtml.checkbox(readOnly.is, t => readOnly(t))) &
     "#add:submit" #> SHtml.submit("Submit", () => processAdd) &
     "#add:cancel" #> SHtml.submit("Cancel", () => S.redirectTo("/"))
   }
@@ -29,7 +31,7 @@ class Gateway {
     import code.model.Mythos._
     Cultist.attending.is match {
       case Full(c) =>
-        val g = gateways.insert(new code.model.Gateway(c.id, host.is.trim + "/" + share.is.trim, path.is.trim, "", password.is.trim, GateMode.rw, GateState.lost))
+        val g = gateways.insert(new code.model.Gateway(c.id, host.is.trim + "/" + share.is.trim, path.is.trim, "", password.is.trim, if (readOnly.is) GateMode.ro else GateMode.rw, GateState.lost))
         // TODO squeryl doesn't have lifecycle callbacks at present, so we must manually trigger event
         Environment.watch(g)
       case _ => S.error("?!")
