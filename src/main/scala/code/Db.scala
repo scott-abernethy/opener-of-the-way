ac17d4331459430cd23b84910ced0da720cf2a52
Scott Abernethy <scott.abernethy@aviatnet.com>
2011-04-01 13:56:25 +1300

    Ooops, wrong adapter was being used in production.

--- src/main/scala/code/Db.scala
+++ src/main/scala/code/Db.scala
@@ -13,17 +13,19 @@ import mapper._
 import code.model._
 
 import org.squeryl._
+import internals.DatabaseAdapter
 import org.squeryl.adapters.H2Adapter
+import org.squeryl.adapters.MySQLAdapter
 
 trait Db {
-  lazy val driver = Props.get("db.driver") openOr "org.h2.Driver"
+  lazy val driver = Props.get("db.driver") openOr "org.squeryl.adapters.H2Adapter"
   lazy val url = Props.get("db.url") openOr "jdbc:h2:test"
   lazy val user = Props.get("db.user") openOr ""
   lazy val password = Props.get("db.password") openOr ""
   def init {
-    Class.forName(driver);
+    val d: DatabaseAdapter = Class.forName(driver).newInstance.asInstanceOf[DatabaseAdapter]
     SessionFactory.concreteFactory = Some(()=>
-      Session.create(java.sql.DriverManager.getConnection(url, user, password), new H2Adapter)
+      Session.create(java.sql.DriverManager.getConnection(url, user, password), d)
     )
   }
   def close {}
