a0cea42d6834ec0aa187557c94fa86f3180309e0
Scott Abernethy <scott.abernethy@aviatnet.com>
2011-04-05 14:42:59 +1200

    Database driver fix

--- src/main/scala/code/Db.scala
+++ src/main/scala/code/Db.scala
@@ -14,18 +14,18 @@ import code.model._
 
 import org.squeryl._
 import internals.DatabaseAdapter
-import org.squeryl.adapters.H2Adapter
-import org.squeryl.adapters.MySQLAdapter
 
 trait Db {
-  lazy val driver = Props.get("db.driver") openOr "org.squeryl.adapters.H2Adapter"
+  lazy val driver = Props.get("db.driver") openOr "org.h2.Driver"
+  lazy val adapter = Props.get("db.adapter") openOr "org.squeryl.adapters.H2Adapter"
   lazy val url = Props.get("db.url") openOr "jdbc:h2:test"
   lazy val user = Props.get("db.user") openOr ""
   lazy val password = Props.get("db.password") openOr ""
   def init {
-    val d: DatabaseAdapter = Class.forName(driver).newInstance.asInstanceOf[DatabaseAdapter]
+    Class.forName(driver)
+    val a: DatabaseAdapter = Class.forName(adapter).newInstance.asInstanceOf[DatabaseAdapter]
     SessionFactory.concreteFactory = Some(()=>
-      Session.create(java.sql.DriverManager.getConnection(url, user, password), d)
+      Session.create(java.sql.DriverManager.getConnection(url, user, password), a)
     )
   }
   def close {}
