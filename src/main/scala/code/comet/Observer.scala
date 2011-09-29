2d16697e183c583a3b5684a600e5a6f9e7ce5370
Scott Abernethy <scott.abernethy@aviatnet.com>
2011-09-29 17:16:21 +1300

    Squeryl madness for from cultist.

--- src/main/scala/code/comet/Observer.scala
+++ src/main/scala/code/comet/Observer.scala
@@ -26,28 +26,30 @@ class Observer extends CometActor with CometListener {
     ".complete-item" #> bindComplete _
   }
   def bindIncomplete(in: NodeSeq): NodeSeq = {
-    incomplete.flatMap(i => bindIncompleteItem(i._1, i._2)(in))
+    incomplete.flatMap(i => bindIncompleteItem(i._1, i._2, i._3)(in))
   }
-  def bindIncompleteItem(clone: Clone, forCultist: Option[Cultist])(in: NodeSeq): NodeSeq = {
+  def bindIncompleteItem(clone: Clone, forCultist: Option[Cultist], fromCultist: Cultist)(in: NodeSeq): NodeSeq = {
     val name = for (c <- forCultist) yield c.sign
     ClearClearable &
     ".incomplete-item [id]" #> idFor(clone.id) &
     ".item-what *" #> clone.id &
     ".item-for *" #> name.getOrElse("?") &
+    ".item-from *" #> fromCultist.sign &
     ".item-requested *" #> DatePresentation.ago(clone.requested.getTime) &
     ".item-attempted *" #> (if (clone.attempts > 0) DatePresentation.ago(clone.attempted.getTime) else "") &
     ".item-state *" #> clone.state.toString &
     ".item-attempts *" #> formatAttempts(clone.attempts) apply(in)
   }
   def bindComplete(in: NodeSeq): NodeSeq = {
-    complete.flatMap(i => bindCompleteItem(i._1, i._2)(in))
+    complete.flatMap(i => bindCompleteItem(i._1, i._2, i._3)(in))
   }
-  def bindCompleteItem(clone: Clone, forCultist: Option[Cultist])(in: NodeSeq): NodeSeq = {
+  def bindCompleteItem(clone: Clone, forCultist: Option[Cultist], fromCultist: Cultist)(in: NodeSeq): NodeSeq = {
     val name = for (c <- forCultist) yield c.sign
     ClearClearable &
     ".complete-item [id]" #> idFor(clone.id) &
     ".item-what *" #> clone.id &
     ".item-for *" #> name.getOrElse("?") &
+    ".item-from *" #> fromCultist.sign &
     ".item-completed *" #> DatePresentation.ago(clone.attempted.getTime) &
     ".item-elapsed *" #> DatePresentation.duration(clone.attempted.getTime - clone.requested.getTime) &
     ".item-duration *" #> formatDurationSeconds(clone.duration) &
@@ -55,19 +57,19 @@ class Observer extends CometActor with CometListener {
   }
   def idFor(id: Long): String = "i" + id
   def incomplete = inTransaction(
-    join(clones, cultists.leftOuter)((c, f) =>
+    join(clones, cultists.leftOuter, artifacts, gateways, cultists)((c, f, a, g, p) =>
       where(c.state === CloneState.awaiting or c.state === CloneState.cloning)
-      select((c, f))
+      select((c, f, p))
       orderBy(c.id desc)
-      on(c.forCultistId === f.map(_.id))
+      on(c.forCultistId === f.map(_.id), c.artifactId === a.id, a.gatewayId === g.id, g.cultistId === p.id)
     ).toList
   )
   def complete = inTransaction(
-    join(clones, cultists.leftOuter)((c, f) =>
+    join(clones, cultists.leftOuter, artifacts, gateways, cultists)((c, f, a, g, p) =>
       where(c.state === CloneState.cloned and c.attempted > T.ago(7 * 24 * 60 * 60 * 1000))
-      select((c, f))
+      select((c, f, p))
       orderBy(c.attempted desc)
-      on(c.forCultistId === f.map(_.id))
+      on(c.forCultistId === f.map(_.id), c.artifactId === a.id, a.gatewayId === g.id, g.cultistId === p.id)
     ).toList
   )
   def formatAttempts(attempts: Long): String = {
