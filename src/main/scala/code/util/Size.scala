56ea0b9f7ffbb1356a2829d98d779d85bc7d2e84
Scott Abernethy <scott.abernethy@aviatnet.com>
2011-11-09 16:10:38 +1300

    Better size display.

--- src/main/scala/code/util/Size.scala
+++ src/main/scala/code/util/Size.scala
@@ -1,5 +1,7 @@
 package code.util
 
+import java.text.DecimalFormat
+
 object Size {
   // 1024s
   // K M G T P
@@ -7,12 +9,28 @@ object Size {
   val units = List("", "K", "M", "G", "T", "P", "E", "Z", "Y")
 
   def short(length: Long): String = {
-    short(length, 0)
+    short(length.toDouble, 0)
   }
 
-  private def short(length: Long, power: Int): String = {
-    if (length < 1024) {
-      length.toString + units(power)
+  private def short(length: Double, power: Int): String = {
+    if (length < 1000) {
+      val rounded: Long = scala.math.round(length)
+      if (rounded < 10)
+      {
+        if (power == 0)
+        {
+          rounded.toString + units(power)
+        }
+        else
+        {
+          // use unrounded and print with 1dp
+          new DecimalFormat("0.0").format(length) + units(power)
+        }
+      }
+      else
+      {
+        rounded.toString + units(power)
+      }
     }
     else {
       short(length / 1024, power + 1)
