20393d65db1cf3d8a71dd8b9742c51bfdebda1ae
Scott Abernethy <scott.abernethy@aviatnet.com>
2013-02-18 11:11:25 +1300

    Version correct

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "opener-of-the-way"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "0.7.0"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
